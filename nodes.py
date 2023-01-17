367af77bff0dfb9f3c120bf76f7495ab148c9821
Maksim Zholudev <maximzms@gmail.com>
2023-01-17 17:04:42 +0300

    Add value node

--- nodes.py
+++ nodes.py
@@ -1,45 +1,327 @@
-from typing import Any
+from typing import Any, Optional
+from enum import Enum, auto
 
-class DataNode:
-    
+
+class ValueNode:
+
+    __value_type: type
+    __value: Any
+    __state: "State"
+    __input: Optional["ProcNode"]
+    __outputs: set["ProcNode"]
+    __build_complete: bool
+    __waiting_outputs: set["ProcNode"]
+   
+    # CONSTRUCTOR
     def __init__(self, value_type: type) -> None:
-        pass
+        self.__value_type = value_type
+        self.__state = self.State.INVALID
+        self.__input = None
+        self.__outputs = set()
+        self.__waiting_outputs = set()
+        self.__build_complete = False
+        self.__add_input_status = self.AddInputStatus.NIL
+        self.__add_output_status = self.AddOutputStatus.NIL
+        self.__put_status = self.PutStatus.NIL
+        self.__invalidate_status = self.InvalidateStatus.NIL
+        self.__used_by_status = self.UsedByStatus.NIL
+        self.__validate_status = self.ValidateStatus.NIL
+        self.__get_state_status = self.GetStateStatus.NIL
+        self.__get_status = self.GetStatus.NIL
 
-    def add_source(self, source: "ProcNode") -> None:
-        pass
 
-    def add_dest(self, source: "ProcNode") -> None:
-        pass
-
-    
-class ProcNode:
-
-    def __init__(self, proc_type: type) -> None:
-        pass
-
-    
     # COMMANDS
 
-    def set_input(self, name: str, node: DataNode) -> None:
-        pass
+    # add input node
+    # PRE: building not complete
+    # PRE: node not linked to this
+    # PRE: this node has no inputs
+    # POST: node linked as input to this
+    def add_input(self, input: "ProcNode") -> None:
+        if self.__build_complete:
+            self.__add_input_status = self.AddInputStatus.BUILD_COMPLETE
+            return
+        if input in self.__outputs or input is self.__input:
+            self.__add_input_status = self.AddInputStatus.ALREADY_LINKED
+            return
+        if self.__input is not None:
+            self.__add_input_status = self.AddInputStatus.TOO_MANY_INPUTS
+            return
+        self.__input = input
+        self.__add_input_status = self.AddInputStatus.OK
 
-    def set_output(self, name: str, node: DataNode) -> None:
-        pass
-
-
-class Input:
-
-    def add(self, name: str, value_type: type) -> None:
-        pass
+    class AddInputStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        ALREADY_LINKED = auto(),
+        TOO_MANY_INPUTS = auto(),
+        BUILD_COMPLETE = auto(),
     
-    def get(self, name: str) -> Any:
-        pass
+    __add_input_status: AddInputStatus
+
+    def get_add_input_status(self) -> AddInputStatus:
+        return self.__add_input_status
 
 
-class Output:
+    # add outinput node
+    # PRE: building not complete
+    # PRE: node not linked to this
+    # POST: node linked as output to this
+    def add_output(self, output: "ProcNode") -> None:
+        if self.__build_complete:
+            self.__add_output_status = self.AddOutputStatus.BUILD_COMPLETE
+            return
+        if output in self.__outputs or output is self.__input:
+            self.__add_output_status = self.AddOutputStatus.ALREADY_LINKED
+            return
+        self.__outputs.add(output)
+        self.__add_output_status = self.AddOutputStatus.OK
 
-    def add(self, name: str, value_type: type) -> None:
-        pass
+    class AddOutputStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        ALREADY_LINKED = auto(),
+        BUILD_COMPLETE = auto(),
+
+    __add_output_status: AddOutputStatus
+
+    def get_add_output_status(self) -> AddOutputStatus:
+        return self.__add_output_status
+
     
-    def put(self, name: str, value: Any) -> None:
+    # complete build
+    # POST: build complete
+    # POST: state is INVALID
+    def complete_build(self) -> None:
+        self.__build_complete = True
+
+
+    # put value
+    # PRE: build complete
+    # PRE: value of proper type
+    # POST: value is set
+    # POST: if there are outputs then state is NEW else REGULAR
+    # POST: all outputs get command input_state_change
+    def put(self, value: Any) -> None:
+        if not self.__build_complete:
+            self.__put_status = self.PutStatus.BUILD_INCOMPLETE
+            return
+        if type(value) is not self.__value_type:
+            self.__put_status = self.PutStatus.INCOMPATIBLE_TYPE
+            return
+        self.__put_status = self.PutStatus.OK
+        self.__value = value
+        self.__state = self.State.REGULAR if len(self.__outputs) == 0 else \
+            self.State.NEW
+        for output in self.__outputs:
+            self.__waiting_outputs.add(output)
+            output.input_state_change()
+            assert(output.get_input_state_change_status() \
+                == ProcNode.InputStateChangeStatus.OK)
+
+    class PutStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        BUILD_INCOMPLETE = auto(),
+        INCOMPATIBLE_TYPE = auto(),
+
+    __put_status: PutStatus
+
+    def get_put_status(self) -> PutStatus:
+        return self.__put_status
+
+
+    # set state to INVALID
+    # PRE: build complete
+    # POST: state is INVALID
+    # POST: if the state has changed then all outputs get command input_state_change
+    def invalidate(self) -> None:
+        if not self.__build_complete:
+            self.__invalidate_status = self.InvalidateStatus.BUILD_INCOMPLETE
+            return
+        self.__invalidate_status = self.InvalidateStatus.OK
+        if self.__state == self.State.INVALID:
+            return
+        self.__state = self.State.INVALID
+        for output in self.__outputs:
+            output.input_state_change()
+            assert(output.get_input_state_change_status() \
+                == ProcNode.InputStateChangeStatus.OK)
+
+    class InvalidateStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        BUILD_INCOMPLETE = auto(),
+
+    __invalidate_status: InvalidateStatus
+
+    def get_invalidate_status(self) -> InvalidateStatus:
+        return self.__invalidate_status
+
+
+    # notify that the value was used by output
+    # PRE: build complete
+    # PRE: output is linked
+    # PRE: not in INVALID state
+    # POST: if state is NEW and all outputs used value then set state to REGULAR
+    def used_by(self, output: "ProcNode") -> None:
+        if not self.__build_complete:
+            self.__used_by_status = self.UsedByStatus.BUILD_INCOMPLETE
+            return
+        if output not in self.__outputs:
+            self.__used_by_status = self.UsedByStatus.NOT_OUTPUT
+            return
+        if self.__state is self.State.INVALID:
+            self.__used_by_status = self.UsedByStatus.INVALID_VALUE
+            return
+        self.__waiting_outputs.remove(output)
+        if len(self.__waiting_outputs) == 0:
+            self.__state = self.State.REGULAR
+        self.__used_by_status = self.UsedByStatus.OK
+
+    class UsedByStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        BUILD_INCOMPLETE = auto(),
+        NOT_OUTPUT = auto(),
+        INVALID_VALUE = auto(),
+
+    __used_by_status: UsedByStatus
+
+    def get_used_by_status(self) -> UsedByStatus:
+        return self.__used_by_status
+
+    
+    # ensure that the value is valid
+    # PRE: build complete
+    # PRE: there is input or the state is not INVALID
+    # POST: if the state is INVALID then the input gets run command
+    def validate(self) -> None:
+        if not self.__build_complete:
+            self.__validate_status = self.ValidateStatus.BUILD_INCOMPLETE
+            return
+        if self.__state == self.State.INVALID and self.__input is None:
+            self.__validate_status = self.ValidateStatus.NO_VALUE_SOURCE
+            return
+        if self.__state == self.State.INVALID:
+            assert(self.__input)
+            self.__input.run()
+            assert(self.__input.get_run_status() == ProcNode.RunStatus.OK)
+        self.__validate_status = self.ValidateStatus.OK
+
+    class ValidateStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        BUILD_INCOMPLETE = auto(),
+        NO_VALUE_SOURCE = auto(),
+
+    __validate_status: ValidateStatus
+
+    def get_validate_status(self) -> ValidateStatus:
+        return self.__validate_status
+
+
+    # QUERIES
+
+    # get input node
+    def get_input(self) -> Optional["ProcNode"]:
+        return self.__input
+
+    # get set of output nodes
+    def get_outputs(self) -> set["ProcNode"]:
+        return self.__outputs
+
+
+    class State(Enum):
+        INVALID = auto(),
+        NEW = auto(),
+        REGULAR = auto(),
+    
+    # get node state
+    # PRE: build complete
+    def get_state(self) -> State:
+        if not self.__build_complete:
+            self.__get_state_status = self.GetStateStatus.BUILD_INCOMPLETE
+            return self.State.INVALID
+        self.__get_state_status = self.GetStateStatus.OK
+        return self.__state
+
+    class GetStateStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        BUILD_INCOMPLETE = auto(),
+
+    __get_state_status: GetStateStatus
+
+    def get_get_state_status(self) -> GetStateStatus:
+        return self.__get_state_status
+
+
+    # get value
+    # PRE: build complete
+    # PRE: state is not INVALID
+    def get(self) -> Any:
+        if not self.__build_complete:
+            self.__get_status = self.GetStatus.BUILD_INCOMPLETE
+            return None
+        if self.__state == self.State.INVALID:
+            self.__get_status = self.GetStatus.INVALID_VALUE
+            return None
+        self.__get_status = self.GetStatus.OK
+        return self.__value
+
+    class GetStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+        BUILD_INCOMPLETE = auto(),
+        INVALID_VALUE = auto(),
+
+    __get_status: GetStatus
+
+    def get_get_status(self) -> GetStatus:
+        return self.__get_status
+
+
+class ProcNode:
+
+    # CONSTRUCTOR
+    def __init__(self, proc_type: type) -> None:
+        self.__input_state_change_status = self.InputStateChangeStatus.NIL
+        self.__run_status = self.RunStatus.NIL
+
+
+    # COMMANDS
+
+    # complete build
+    # POST: procedure initialized
+    # POST: build complete
+    def complete_build(self) -> None:
         pass
+
+    
+    # process update of input state change to NEW or INVALID
+    def input_state_change(self) -> None:
+        self.__input_state_change_status = self.InputStateChangeStatus.OK
+
+    class InputStateChangeStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+
+    __input_state_change_status: InputStateChangeStatus
+
+    def get_input_state_change_status(self) -> InputStateChangeStatus:
+        return self.__input_state_change_status
+
+    
+    # run the procedure
+    def run(self) -> None:
+        self.__run_status = self.RunStatus.OK
+
+    class RunStatus(Enum):
+        NIL = auto(),
+        OK = auto(),
+
+    __run_status: RunStatus
+
+    def get_run_status(self) -> RunStatus:
+        return self.__run_status
--- test_nodes.py
+++ test_nodes.py
@@ -1,35 +1,178 @@
 import unittest
 
-from nodes import ProcNode, DataNode, Input, Output
-
-class AddProc:
-
-    def __init__(self, input: Input, output: Output) -> None:
-        self.__input = input
-        self.__output = output
-        self.__input.add("arg1", int)
-        self.__input.add("arg2", int)
-        self.__output.add("result", int)
-
-    def run(self) -> None:
-        arg1 = self.__input.get("arg1")
-        arg2 = self.__input.get("arg2")
-        self.__output.put("result", arg1 + arg2)
+from nodes import ValueNode, ProcNode
 
 
-class Test(unittest.TestCase):
+class Test_Value(unittest.TestCase):
 
-    def test(self):
-        a = DataNode(int)
-        b = DataNode(int)
-        c = DataNode(int)
-        p = ProcNode(AddProc)
-        p.set_input("left", a)
-        a.add_dest(p)
-        p.set_input("right", b)
-        b.add_dest(p)
-        p.set_output("result", c)
-        c.add_source(p)
+    def test_build(self):
+        v1 = ValueNode(int)
+        p1 = ProcNode(object)
+        p2 = ProcNode(object)
+        p3 = ProcNode(object)
+        p4 = ProcNode(object)
+        self.assertEqual(v1.get_add_input_status(), ValueNode.AddInputStatus.NIL)
+        self.assertEqual(v1.get_add_output_status(), ValueNode.AddOutputStatus.NIL)
+        self.assertIsNone(v1.get_input())
+        self.assertEqual(v1.get_outputs(), set())
+        v1.add_output(p2)
+        self.assertEqual(v1.get_add_output_status(), ValueNode.AddOutputStatus.OK)
+        v1.add_output(p2)
+        self.assertEqual(v1.get_add_output_status(), ValueNode.AddOutputStatus.ALREADY_LINKED)
+        v1.add_input(p2)
+        self.assertEqual(v1.get_add_input_status(), ValueNode.AddInputStatus.ALREADY_LINKED)
+        v1.add_input(p1)
+        self.assertEqual(v1.get_add_input_status(), ValueNode.AddInputStatus.OK)
+        v1.add_input(p1)
+        self.assertEqual(v1.get_add_input_status(), ValueNode.AddInputStatus.ALREADY_LINKED)
+        v1.add_input(p3)
+        self.assertEqual(v1.get_add_input_status(), ValueNode.AddInputStatus.TOO_MANY_INPUTS)
+        v1.add_output(p1)
+        self.assertEqual(v1.get_add_output_status(), ValueNode.AddOutputStatus.ALREADY_LINKED)
+        v1.add_output(p3)
+        self.assertEqual(v1.get_add_output_status(), ValueNode.AddOutputStatus.OK)
+        v1.complete_build()
+        v1.add_input(p4)
+        self.assertEqual(v1.get_add_input_status(), ValueNode.AddInputStatus.BUILD_COMPLETE)
+        v1.add_output(p4)
+        self.assertEqual(v1.get_add_output_status(), ValueNode.AddOutputStatus.BUILD_COMPLETE)
+        self.assertEqual(v1.get_input(), p1)
+        self.assertEqual(v1.get_outputs(), {p2, p3})
+
+
+    def test_get_state(self):
+        v1 = ValueNode(int)
+        self.assertEqual(v1.get_get_state_status(), ValueNode.GetStateStatus.NIL)
+        v1.get_state()
+        self.assertEqual(v1.get_get_state_status(), ValueNode.GetStateStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        self.assertEqual(v1.get_get_state_status(), ValueNode.GetStateStatus.OK)
+
+
+    def test_put_last(self):
+        v1 = ValueNode(int)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.NIL)
+        v1.put(1)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        v1.put(0.5)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.INCOMPATIBLE_TYPE)
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        v1.put(1)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.OK)
+        self.assertEqual(v1.get_state(), ValueNode.State.REGULAR)
+
+
+    def test_put_mid(self):
+        v1 = ValueNode(int)
+        p1 = ProcNode(object)
+        v1.add_output(p1)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.NIL)
+        v1.put(1)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        self.assertEqual(p1.get_input_state_change_status(), ProcNode.InputStateChangeStatus.NIL)
+        v1.put(0.5)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.INCOMPATIBLE_TYPE)
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        self.assertEqual(p1.get_input_state_change_status(), ProcNode.InputStateChangeStatus.NIL)
+        v1.put(1)
+        self.assertEqual(v1.get_put_status(), ValueNode.PutStatus.OK)
+        self.assertEqual(v1.get_state(), ValueNode.State.NEW)
+        self.assertEqual(p1.get_input_state_change_status(), ProcNode.InputStateChangeStatus.OK)
+
+
+    def test_invalidate(self):
+        v1 = ValueNode(int)
+        p1 = ProcNode(object)
+        v1.add_output(p1)
+        p1.complete_build()
+        self.assertEqual(v1.get_invalidate_status(), ValueNode.InvalidateStatus.NIL)
+        v1.invalidate()
+        self.assertEqual(v1.get_invalidate_status(), ValueNode.InvalidateStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        self.assertEqual(p1.get_input_state_change_status(), ProcNode.InputStateChangeStatus.NIL)
+        v1.invalidate()
+        self.assertEqual(v1.get_invalidate_status(), ValueNode.InvalidateStatus.OK)
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        self.assertEqual(p1.get_input_state_change_status(), ProcNode.InputStateChangeStatus.NIL)
+        v1.put(1)
+        self.assertEqual(v1.get_state(), ValueNode.State.NEW)
+        v1.invalidate()
+        self.assertEqual(v1.get_invalidate_status(), ValueNode.InvalidateStatus.OK)
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        self.assertEqual(p1.get_input_state_change_status(), ProcNode.InputStateChangeStatus.OK)
+
+    
+    def test_used(self):
+        v1 = ValueNode(int)
+        p1 = ProcNode(object)
+        p2 = ProcNode(object)
+        p3 = ProcNode(object)
+        v1.add_output(p1)
+        v1.add_output(p2)
+        self.assertEqual(v1.get_used_by_status(), ValueNode.UsedByStatus.NIL)
+        v1.used_by(p1)
+        self.assertEqual(v1.get_used_by_status(), ValueNode.UsedByStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        self.assertEqual(v1.get_state(), ValueNode.State.INVALID)
+        v1.used_by(p1)
+        self.assertEqual(v1.get_used_by_status(), ValueNode.UsedByStatus.INVALID_VALUE)
+        v1.put(1)
+        self.assertEqual(v1.get_state(), ValueNode.State.NEW)
+        v1.used_by(p1)
+        self.assertEqual(v1.get_used_by_status(), ValueNode.UsedByStatus.OK)
+        self.assertEqual(v1.get_state(), ValueNode.State.NEW)
+        v1.used_by(p3)
+        self.assertEqual(v1.get_used_by_status(), ValueNode.UsedByStatus.NOT_OUTPUT)
+        self.assertEqual(v1.get_state(), ValueNode.State.NEW)
+        v1.used_by(p2)
+        self.assertEqual(v1.get_used_by_status(), ValueNode.UsedByStatus.OK)
+        self.assertEqual(v1.get_state(), ValueNode.State.REGULAR)
+
+
+    def test_validate_first(self):
+        v1 = ValueNode(int)
+        self.assertEqual(v1.get_validate_status(), ValueNode.ValidateStatus.NIL)
+        v1.validate()
+        self.assertEqual(v1.get_validate_status(), ValueNode.ValidateStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        v1.validate()
+        self.assertEqual(v1.get_validate_status(), ValueNode.ValidateStatus.NO_VALUE_SOURCE)
+        v1.put(1)
+        v1.validate()
+        self.assertEqual(v1.get_validate_status(), ValueNode.ValidateStatus.OK)
+
+
+    def test_validate_mid(self):
+        v1 = ValueNode(int)
+        p1 = ProcNode(int)
+        v1.add_input(p1)
+        self.assertEqual(v1.get_validate_status(), ValueNode.ValidateStatus.NIL)
+        v1.validate()
+        self.assertEqual(v1.get_validate_status(), ValueNode.ValidateStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        self.assertEqual(p1.get_run_status(), ProcNode.RunStatus.NIL)
+        v1.validate()
+        self.assertEqual(v1.get_validate_status(), ValueNode.ValidateStatus.OK)
+        self.assertEqual(p1.get_run_status(), ProcNode.RunStatus.OK)
+
+
+    def test_get(self):
+        v1 = ValueNode(int)
+        self.assertEqual(v1.get_get_status(), ValueNode.GetStatus.NIL)
+        v1.get()
+        self.assertEqual(v1.get_get_status(), ValueNode.GetStatus.BUILD_INCOMPLETE)
+        v1.complete_build()
+        v1.get()
+        self.assertEqual(v1.get_get_status(), ValueNode.GetStatus.INVALID_VALUE)
+        v1.put(1)
+        self.assertEqual(v1.get(), 1)
+        self.assertEqual(v1.get_get_status(), ValueNode.GetStatus.OK)
 
 
 if __name__ == "__main__":
