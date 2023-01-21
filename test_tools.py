04316c5a5df3e85b8888f72e567d6e75abd6fd51
Maksim Zholudev <maximzms@gmail.com>
2023-01-22 00:30:16 +0300

    Improve test

--- test_tools.py
+++ test_tools.py
@@ -4,17 +4,25 @@ from tools import Status, status
 
 class Foo(Status):
     @status("OK", "ERR")
-    def func_ok(self) -> None:
-        self._set_status("func_ok", "OK")
+    def func(self, s: str) -> None:
+        self._set_status("func", s)
 
 
 class Test_Status(unittest.TestCase):
 
-    def test(self):
+    def test_set_get(self):
         foo = Foo()
-        self.assertEqual(foo.get_status("func_ok"), "NIL")
-        foo.func_ok()
-        self.assertEqual(foo.get_status("func_ok"), "OK")
+        self.assertEqual(foo.get_status("func"), "NIL")
+        foo.func("OK")
+        self.assertEqual(foo.get_status("func"), "OK")
+        foo.func("ERR")
+        self.assertEqual(foo.get_status("func"), "ERR")
+
+        foo2 = Foo()
+        self.assertEqual(foo2.get_status("func"), "NIL")
+        foo2.func("OK")
+        self.assertEqual(foo2.get_status("func"), "OK")
+        self.assertEqual(foo.get_status("func"), "ERR")
 
 
 if __name__ == "__main__":
