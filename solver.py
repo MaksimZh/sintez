e4edcfa33da7ca9125540268386aa9078b39533f
Maksim Zholudev <maximzms@gmail.com>
2023-02-16 17:08:15 +0300

    Add FuncSpec

--- solver.py
+++ solver.py
@@ -91,19 +91,17 @@ class SolverFactory(ABC):
 # Solver wrapping function
 class WrapperSolver(Solver):
 
-    __func: Func
-    __input_spec: dict[str, type]
-    __output_spec: dict[str, type]
+    __spec: "FuncSpec"
     __inputs: dict[str, Any]
+    __outputs: dict[str, Any]
 
 
     # CONSTRUCTOR
-    def __init__(self, func: Func, 
-            input_spec: dict[str, type], output_spec: dict[str, type]) -> None:
+    def __init__(self, spec: "FuncSpec") -> None:
         super().__init__()
-        self.__func = func
-        self.__input_spec = input_spec
-        self.__output_spec = output_spec
+        self.__spec = spec
+        self.__inputs = dict()
+        self.__outputs = dict()
 
     
     # COMMANDS
@@ -114,12 +112,13 @@ class WrapperSolver(Solver):
     # POST: input `id` is equal to `value`
     @status("OK", "INVALID_ID", "INVALID_VALUE")
     def put(self, id: str, value: Any) -> None:
-        if not id in self.__input_spec:
+        if not id in self.__spec.get_input_spec():
             self._set_status("put", "INVALID_ID")
             return
-        if not _type_fits(type(value), self.__input_spec[id]):
+        if not _type_fits(type(value), self.__spec.get_input_spec()[id]):
             self._set_status("put", "INVALID_VALUE")
             return
+        self.__inputs[id] = value
         self._set_status("put", "OK")
 
     # Run solver
@@ -128,6 +127,28 @@ class WrapperSolver(Solver):
     # POST: output values are set
     @status("OK", "INVALID_INPUT", "INTERNAL_ERROR")
     def run(self) -> None:
+        args = list[Any]()
+        for id in self.__spec.get_input_ids():
+            if id not in self.__inputs:
+                self._set_status("run", "INVALID_INPUT")
+                return
+            args.append(self.__inputs[id])
+        try:
+            result = self.__spec.get_func()(*args)
+        except:
+            self._set_status("run", "INTERNAL_ERROR")
+            return
+        output_ids = self.__spec.get_output_ids()
+        output_spec = self.__spec.get_output_spec()
+        if len(output_ids) == 1:
+            result = tuple(result,)
+        for i in range(len(output_ids)):
+            value = result[i]
+            id = output_ids[i]
+            if not _type_fits(type(value), output_spec[id]):
+                self._set_status("run", "INTERNAL_ERROR")
+                return
+            self.__outputs[id] = value
         self._set_status("run", "OK")  
 
 
@@ -135,11 +156,11 @@ class WrapperSolver(Solver):
     
     # Get input value ids and types
     def get_input_spec(self) -> dict[str, type]:
-        return self.__input_spec
+        return self.__spec.get_input_spec()
 
     # Get output value ids and types
     def get_output_spec(self) -> dict[str, type]:
-        return self.__output_spec
+        return self.__spec.get_output_spec()
 
     # Check if input or output value is set
     # PRE: `id` is valid input or output name
@@ -162,9 +183,7 @@ class WrapperSolver(Solver):
 #   - output types
 class Wrapper(SolverFactory):
 
-    __func: Func
-    __input_spec: dict[str, type]
-    __output_spec: dict[str, type]
+    __spec: "FuncSpec"
 
 
     # CONSTRUCTOR
@@ -177,11 +196,49 @@ class Wrapper(SolverFactory):
     # POST: output types are return tuple type elements
     def __init__(self, func: Func, output_ids: list[str]) -> None:
         super().__init__()
+        self.__spec = FuncSpec(func, output_ids)
+
+    # Create solver
+    # POST: `Solver` inputs and outputs have no values
+    def create(self) -> Solver:
+        return WrapperSolver(self.__spec)
+
+    # Get solver input value ids and types
+    def get_input_spec(self) -> dict[str, type]:
+        return self.__spec.get_input_spec()
+
+    # Get solver output value ids and types
+    def get_output_spec(self) -> dict[str, type]:
+        return self.__spec.get_output_spec()
+
+
+def _type_fits(t: type, required: type) -> bool:
+    if issubclass(t, required):
+        return True
+    if required is complex:
+        return t is int or t is float
+    if required is float:
+        return t is int
+    return False
+
+
+class FuncSpec:
+
+    __func: Func
+    __input_ids: list[str]
+    __output_ids: list[str]
+    __input_spec: dict[str, type]
+    __output_spec: dict[str, type]
+
+    def __init__(self, func: Func, output_ids: list[str]) -> None:
         self.__func = func
         arg_spec = getfullargspec(func)
+        self.__input_ids = list()
+        self.__output_ids = output_ids
         self.__input_spec = dict()
         self.__output_spec = dict()
         for id in arg_spec.args:
+            self.__input_ids.append(id)
             self.__input_spec[id] = arg_spec.annotations[id]
         if "return" not in arg_spec.annotations:
             assert len(output_ids) == 0
@@ -195,25 +252,17 @@ class Wrapper(SolverFactory):
         for i in range(len(output_ids)):
             self.__output_spec[output_ids[i]] = output_types[i]
 
-    # Create solver
-    # POST: `Solver` inputs and outputs have no values
-    def create(self) -> Solver:
-        return WrapperSolver(self.__func, self.__input_spec, self.__output_spec)
+    def get_func(self) -> Func:
+        return self.__func
 
-    # Get solver input value ids and types
+    def get_input_ids(self) -> list[str]:
+        return self.__input_ids
+    
+    def get_output_ids(self) -> list[str]:
+        return self.__output_ids
+
     def get_input_spec(self) -> dict[str, type]:
         return self.__input_spec
-
-    # Get solver output value ids and types
+    
     def get_output_spec(self) -> dict[str, type]:
         return self.__output_spec
-
-
-def _type_fits(t: type, required: type) -> bool:
-    if issubclass(t, required):
-        return True
-    if required is complex:
-        return t is int or t is float
-    if required is float:
-        return t is int
-    return False
